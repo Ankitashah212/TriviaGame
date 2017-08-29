ac98983e7e8e6d712b8581470b9be1703b19e44c
Kita <ankitashah212@gmail.com>
2017-08-28 22:12:20 -0400

    basic html structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+//create an object for question with possible answers
+$(document).ready(function () {
+
+    $("#start").on("click", function () {
+        //reset text for score
+        $('#startDiv').css("display", "none");
+        $('#mainContent').show();
+      
+    });
+
+});
